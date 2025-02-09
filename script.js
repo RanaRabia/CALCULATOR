a7b4ded7deaace5c56af3f882ac7bb5fa40ace8a
rabia-rana <shayanrana696@gmail.com>
2025-02-10 03:49:58 +0500

    calculator using html

---
+++ script.js
@@ -0,0 +1,22 @@
+function calculate() {
+    const firstNumber = parseFloat(document.getElementById("firstNumber").value)
+    const secondNumber = parseFloat(document.getElementById("secondNumber").value)
+const operator= document.getElementById("operator").value
+let result
+if(operator === "Addition"){
+    result = firstNumber + secondNumber
+}
+else if (operator === "Subtraction"){
+    result = firstNumber - secondNumber
+}
+else if (operator === "Multiplication"){
+    result = firstNumber * secondNumber
+}
+else if (operator === "Division"){
+    result = firstNumber / secondNumber
+}
+else {
+    result="Please Select a Valid Operator"
+}
+document.getElementById("result").innerText= "Result:" + result;
+}
